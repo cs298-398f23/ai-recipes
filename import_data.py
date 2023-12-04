74a1a95f275d062779414108f9927b6a45affdb3
elon-shmusk <59424740+elon-shmusk@users.noreply.github.com>
2023-12-04 08:19:24 -0500

    Update import_data.py DB_NAME -> DB_DATABASE

--- import_data.py
+++ import_data.py
@@ -11,7 +11,7 @@ db_config = {
     'host': os.getenv('DB_HOST'),
     'user': os.getenv('DB_USER'),
     'password': os.getenv('DB_PASSWORD'),
-    'database': os.getenv('DB_NAME'),
+    'database': os.getenv('DB_DATABASE'),
 }
 
 # Function to connect to the MySQL database
